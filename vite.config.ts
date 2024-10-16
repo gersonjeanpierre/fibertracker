5d9d4c200b4f0c97db9d889de7af55ad28584713
JalukOne <jalukone@outlook.com>
2024-10-16 15:24:29 -0500

    hotfix GHPages fix client-side

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { vitePlugin as remix } from "@remix-run/dev";
+import { copyFileSync } from "node:fs";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
@@ -12,6 +13,23 @@ export default defineConfig({
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
+      },
+      buildEnd(args) {
+        if (!args.viteConfig.isProduction) return;
+
+        // When deploying to GitHub Pages, if you navigate from / to another
+        // route and refresh the tab, it will show the default GH Pages 404
+        // page. This happens because GH Pages is not configured to send all
+        // traffic to the index.html where we can load our client-side JS.
+        // To fix this, we can create a 404.html file that contains the same
+        // content as index.html. This way, when the user refreshes the page,
+        // GH Pages will serve our 404.html and everything will work as
+        //expected.
+        const buildPath = args.viteConfig.build.outDir;
+        copyFileSync(
+          join(buildPath, "index.html"),
+          join(buildPath, "404.html")
+        );
       },
     }),
     tsconfigPaths(),
